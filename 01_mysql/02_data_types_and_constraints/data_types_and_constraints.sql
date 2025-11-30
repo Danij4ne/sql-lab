7812413b8172799638d22fbae537a2ad98caf964
Daniel Jane <danij4ne@gmail.com>
2025-11-30 22:36:30 +0100

    Add SQL script covering data types and constraints

---
+++ 01_mysql/02_data_types_and_constraints/data_types_and_constraints.sql
@@ -0,0 +1,23 @@
+
+-- Basic unit of measure = tables
+
+-- In SQL, for each column we must indicate the data type of the values it will store.
+
+-- Basic data types:
+-- int      = number
+-- varchar  = text
+-- decimal  = decimal number
+-- datetime = date with time
+-- DATE     = date without time
+
+-- Common column attributes / constraints:
+-- PK = PRIMARY KEY -> the unique id (all of this can also be done using code)
+-- NN = NOT NULL    -> force a column to not be empty
+-- UQ = UNIQUE      -> value cannot be repeated
+-- B  = BINARY      -> binary value
+-- UN = UNSIGNED    -> unsigned (no negative values)
+-- ZF = ZEROFILL    -> pads the number with zeros
+-- AI = AUTO_INCREMENT -> auto-increment
+-- G  = GENERATED COLUMN
+
+-- VARCHAR(50) -> can only store up to 50 characters in a string
