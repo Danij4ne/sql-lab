8a9760e5606eb0e0bc850c567582a076b6281e6b
Daniel Jane <danij4ne@gmail.com>
2025-11-30 22:39:46 +0100

    Add INSERT statement examples

---
+++ 01_mysql/21_insert/insert.sql
@@ -0,0 +1,11 @@
+
+-------------------------------------- DATA WRITING ---------------------------------------------------------
+-- Commands that actually modify the database
+
+-- INSERT INTO = insert data into a table
+-- VALUES()    = the values that the row will have
+
+INSERT INTO users (user_id, name, surname)
+VALUES (7, 'Maria', 'Lopez');
+-- Insert into the users table (user_id, name, surname)
+-- the VALUES (7, 'Maria', 'Lopez').
