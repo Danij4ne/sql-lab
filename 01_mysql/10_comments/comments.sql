8a6d482f4d047eb74184c4c8b7afe979cca61d4f
Daniel Jane <danij4ne@gmail.com>
2025-11-30 22:38:03 +0100

    Add SQL comments examples

---
+++ 01_mysql/10_comments/comments.sql
@@ -0,0 +1,13 @@
+
+--------------------------------------------------------------
+-- Comments in SQL (Modifiers Part 2)
+--------------------------------------------------------------
+
+-- How to write a comment in SQL?
+
+-- 1) Single line:
+--    -- Hello this is my comment
+
+-- 2) Multiple lines:
+--    /* My other
+--       comment                */
