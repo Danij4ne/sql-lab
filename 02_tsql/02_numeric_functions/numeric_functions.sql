5770689a2a0e195cc8603b4bef49c7e3d7dcddfb
Daniel Jane <danij4ne@gmail.com>
2025-11-30 22:44:51 +0100

    Add T-SQL numeric functions examples

---
+++ 02_tsql/02_numeric_functions/numeric_functions.sql
@@ -0,0 +1,9 @@
+
+----------------
+-- ROUND() = round numbers
+----------------
+SELECT ROUND(Price, 2) AS Rounded_Price
+FROM Products;
+-- ROUND() = round a number
+-- Price = column
+-- 2 = number of decimals to keep
