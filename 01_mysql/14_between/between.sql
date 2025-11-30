41c99f40dbd2265498678320b993da001d1c8735
Daniel Jane <danij4ne@gmail.com>
2025-11-30 22:38:44 +0100

    Add BETWEEN operator examples

---
+++ 01_mysql/14_between/between.sql
@@ -0,0 +1,11 @@
+
+-----------------------------------------------------------------
+-- BETWEEN
+-----------------------------------------------------------------
+
+-- BETWEEN = find results between two values (inclusive)
+
+SELECT * 
+FROM users 
+WHERE age BETWEEN 20 AND 30;
+-- Select all rows from users where age is between 20 and 30
